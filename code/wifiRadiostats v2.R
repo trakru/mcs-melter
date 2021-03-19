1522daffbbb170b70079c54c7b7c378fc242dcb8
Ankush Trakru <ankush.trakru@commscope.com>
2021-03-18 22:33:27 -0400

    some cleanup on the R script

--- code/wifiRadiostats v2.R
+++ code/wifiRadiostats v2.R
@@ -1,7 +1,11 @@
-library(drake)
+# this code actually requires the radiostats file, not radiostats mcs. Any attempt to make it run on the current raw file in the data folder will fail
+
+library(here)
 library(lubridate)
 library(tidyverse)
 library(reshape)
+
+file_location = here('data', 'raw', 'OG1600.WiFiRadioStats.2020_03_01.0.csv')
 
 raw_data  <- read_csv('F:/Dashboarding/SharePoint/CommScope/XWiFi XM Offload - og1600Logs/15 min/2-arrisRouterWiFiRadioStats/OG1600.WiFiRadioStats.2020_06_13.csv', 
                       col_types = cols(`2.4_RadioACKFailed` = col_number(), 
@@ -117,88 +121,4 @@ df_melted                                   <- df_melted %>% mutate(radio_type =
 
 
 # writing CSVs
-# write.csv(df2_by_day, './data/pickles/mcs_06_15_2020.csv')
 write.csv(df_melted, './data/melted/mcs_06_13_2020_melted.csv')
-# write.csv(df_FACT, './data/pickles/mcs_06_15_2020_FACT.csv')
-# write.csv(df_LOCATION, './data/pickles/mcs_06_15_2020_LOCATION.csv')
-
-# colnames(df_melted)
-
-# #plotting
-# library(plotly)
-# 
-# fig                                       <- plot_ly(
-#   x = df2$Current_Location,
-#   y = df2$Rx_mcs0,
-#   name = "mcs0",
-#   type = "bar"
-# )
-# 
-# fig                                       <- fig %>%
-#   add_trace(y = ~df2$Rx_mcs1, type = 'bar', name='mcs1') %>%
-#   add_trace(y = ~df2$Rx_mcs2, type = 'bar', name='mcs2') %>%
-#   add_trace(y = ~df2$Rx_mcs3, type = 'bar', name='mcs3') %>%
-#   add_trace(y = ~df2$Rx_mcs4, type = 'bar', name='mcs4') %>%
-#   add_trace(y = ~df2$Rx_mcs5, type = 'bar', name='mcs5') %>%
-#   add_trace(y = ~df2$Rx_mcs6, type = 'bar', name='mcs6') %>%
-#   add_trace(y = ~df2$Rx_mcs7, type = 'bar', name='mcs7') %>%
-#   add_trace(y = ~df2$Rx_mcs8, type = 'bar', name='mcs8') %>%
-#   add_trace(y = ~df2$Rx_mcs9, type = 'bar', name='mcs9') %>%
-#   add_trace(y = ~df2$Rx_mcs10, type = 'bar', name='mcs10') %>%
-#   add_trace(y = ~df2$Rx_mcs11, type = 'bar', name='mcs11') %>%
-#   add_trace(y = ~df2$Rx_mcs12, type = 'bar', name='mcs12') %>%
-#   add_trace(y = ~df2$Rx_mcs13, type = 'bar', name='mcs13') %>%
-#   add_trace(y = ~df2$Rx_mcs14, type = 'bar', name='mcs14') %>%
-#   add_trace(y = ~df2$Rx_mcs15, type = 'bar', name='mcs15') %>%
-#   add_trace(y = ~df2$Rx_mcs16, type = 'bar', name='mcs16') %>%
-#   add_trace(y = ~df2$Rx_mcs17, type = 'bar', name='mcs17') %>%
-#   add_trace(y = ~df2$Rx_mcs18, type = 'bar', name='mcs18') %>%
-#   add_trace(y = ~df2$Rx_mcs19, type = 'bar', name='mcs19') %>%
-#   add_trace(y = ~df2$Rx_mcs20, type = 'bar', name='mcs20') %>%
-#   add_trace(y = ~df2$Rx_mcs21, type = 'bar', name='mcs21') %>%
-#   add_trace(y = ~df2$Rx_mcs22, type = 'bar', name='mcs22') %>%
-#   add_trace(y = ~df2$Rx_mcs23, type = 'bar', name='mcs23') %>%
-#   layout(yaxis = list(title = 'sum of MCS'), barmode = 'group')
-# 
-# 
-# fig
-# 
-# updatemenus                               <- list(
-#   list(
-#     active = -1,
-#     type= 'buttons',
-#     buttons = list(
-#       list(
-#         label = "High",
-#         method = "update",
-#         args = list(list(visible = c(FALSE, TRUE)),
-#                     list(title = "Apple High",
-#                          annotations = list(c(), high_annotations)))),
-#       list(
-#         label = "Low",
-#         method = "update",
-#         args = list(list(visible = c(TRUE, FALSE)),
-#                     list(title = "Apple Low",
-#                          annotations = list(low_annotations, c() )))),
-#       list(
-#         label = "Both",
-#         method = "update",
-#         args = list(list(visible = c(TRUE, TRUE)),
-#                     list(title = "Apple",
-#                          annotations = list(low_annotations, high_annotations)))),
-#       list(
-#         label = "Reset",
-#         method = "update",
-#         args = list(list(visible = c(TRUE, TRUE)),
-#                     list(title = "Apple",
-#                          annotations = list(c(), c())))))
-#   )
-# )
-# 
-
-
-
-
-# 
-# df5G                                      <- raw_data %>% select(Timestamp, Current_IP, Current_MAC, Current_Location, `5G_RadioRxbyrate`, `5G_RadioTxbyrate`)
-# df5G                                      <- df5G %>% filter(`5G_RadioRxbyrate` != "na")
